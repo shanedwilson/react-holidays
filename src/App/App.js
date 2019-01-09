43ab6eb181ad645d551eada76c127da38312b0d4
Shane D Wilson <me@shanedwilson.com>
2019-01-08 23:44:38 -0600

    navbar done

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,84 @@
-import React, { Component } from 'react';
+import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+import Auth from '../components/pages/Auth/Auth';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
+import authRequests from '../helpers/data/authRequests';
+import connection from '../helpers/data/connection';
+
 import './App.scss';
 
-class App extends Component {
-  render() {
-    return (
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (authed === false
+    ? (<Component { ...props } />)
+    : (<Redirect to={{ pathname: '#', state: { from: props.location } }}/>));
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
+
+class App extends React.Component {
+    state = {
+      authed: false,
+      pendingUser: true,
+    }
+
+    componentDidMount() {
+      connection();
+      this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          this.setState({
+            authed: true,
+            pendingUser: false,
+          });
+        } else {
+          this.setState({
+            authed: false,
+            pendingUser: false,
+          });
+        }
+      });
+    }
+
+    componentWillUnmount() {
+      this.removeListener();
+    }
+
+    render() {
+      const {
+        authed,
+        pendingUser,
+      } = this.state;
+
+      const logoutClickEvent = () => {
+        authRequests.logoutUser();
+        this.setState({ authed: false });
+      };
+
+      if (pendingUser) {
+        return null;
+      }
+
+      return (
       <div className="App">
-        <h1>App Component</h1>
-        <button className="btn btn-danger">BUTTON!</button>
-      </div>
-    );
-  }
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavbar isAuthed={ authed } logoutClickEvent={logoutClickEvent} />
+            <div className="container">
+              <div className='row'>
+                <Switch>
+                  <PublicRoute path='/auth' component={Auth} authed={this.state.authed} />
+                </Switch>
+              </div>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>      </div>
+      );
+    }
 }
 
 export default App;
---
+++ src/components/MyNavbar/MyNavbar.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink as RRNavLink } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+import './MyNavbar.scss';
+
+class MyNavbar extends React.Component {
+  static propTypes = {
+    isAuthed: PropTypes.bool,
+    logoutClickEvent: PropTypes.func,
+  }
+
+  state = {
+    isOpen: false,
+  };
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  render() {
+    const { isAuthed, logoutClickEvent } = this.props;
+    const buildNavbar = () => {
+      if (isAuthed) {
+        return (
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink tag={RRNavLink} to="/friends">Friends</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={RRNavLink} to="/newfriend">Add Friend</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={RRNavLink} to="/holidays">Holidays</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={RRNavLink} to="/newholiday">Add Holiday</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink onClick={logoutClickEvent}>Logout</NavLink>
+          </NavItem>
+        </Nav>
+        );
+      }
+      return <Nav className="ml-auto" navbar />;
+    };
+
+    return (
+      <div className="my-navbar">
+       <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/">React-Holidays</NavbarBrand>
+          <NavbarToggler onClick={e => this.toggle(e)} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+           {buildNavbar()}
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default MyNavbar;
