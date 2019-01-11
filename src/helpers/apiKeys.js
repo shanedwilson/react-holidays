a0d58015635e8519e131f73fa5803c35b9940c0b
Shane D Wilson <me@shanedwilson.com>
2019-01-11 13:54:07 -0600

    wip

---
+++ src/helpers/apiKeys.js
@@ -0,0 +1,12 @@
+const apiKeys = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyBS3nRhh4GHpQ5YFCoMQo2poqB3mEqjSO4',
+    authDomain: 'react-holidays.firebaseapp.com',
+    databaseURL: 'https://react-holidays.firebaseio.com',
+    projectId: 'react-holidays',
+    storageBucket: '',
+    messagingSenderId: '313961860471',
+  },
+};
+
+export default apiKeys;
