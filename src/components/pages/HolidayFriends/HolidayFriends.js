3fdf0989b94cd319855eb76c6a43007d39e85e4d
Shane D Wilson <me@shanedwilson.com>
2019-01-17 00:45:51 -0600

    holiday friends/notFriends rendering with correct checkbox status

--- src/components/pages/HolidayFriends/HolidayFriends.js
+++ src/components/pages/HolidayFriends/HolidayFriends.js
@@ -10,6 +10,7 @@ class HolidayFriends extends React.Component {
     singleHoliday: [],
     friends: [],
     holidayFriends: [],
+    notHolidayFriends: [],
     checked: false,
   }
 
@@ -37,119 +38,50 @@ class HolidayFriends extends React.Component {
         this.setState({ holidayFriends });
       });
     });
+
+    holidayFriendsData.getFriendIdsForHoliday(firebaseId).then((friendIds) => {
+      friendsData.getNotFriendsByArrayOfIds(uid, friendIds).then((notHolidayFriends) => {
+        this.setState({ notHolidayFriends });
+      });
+    });
   }
 
   checkEvent = (e) => {
-    console.log('HEY!!!');
     this.setState({ checked: e.target.checked });
   }
 
   render() {
     const {
       singleHoliday,
-      friends,
       holidayFriends,
+      notHolidayFriends,
     } = this.state;
 
-    // {
-    //   this.props.items
-    //     .forEach(item => items.push(
-    //                    <li>
-    //                       <TodoItem id={item.id} key={item.id} text={item.text} />
-    //                    </li>
-    //     ))
-
-    //   return(
-    //  <ul>{items}</ul>
-    //   );
-    // }
-
-    const friendsAttending = [];
-    const friendsNotAttending = [];
-
-    // const makeArrays = friends.forEach((friend) => {
-    //   holidayFriends.forEach((holidayFriend) => {
-    //     if (holidayFriend.id === friend.id) {
-    //       friendsAttending.push(
-    //       <div key={friend.id} className="card col-3 mt-3 mr-1">
-    //         <h5 className="card-header">{friend.name}</h5>
-    //         <div className="card-body">
-    //           <input type="checkbox" checked='true' className="form-check-input" id="attendingCheck" onChange={this.checkEvent} />
-    //           <label className="form-check-label" htmlFor="exampleCheck1">Attending {singleHoliday.name}?</label>
-    //         </div>
-    //       </div>,
-    //       );
-    //       console.log(friendsAttending);
-    //     } else {
-    //       friendsNotAttending.push(
-    //       <div key={friend.id} className="card col-3 mt-3 mr-1">
-    //         <h5 className="card-header">{friend.name}</h5>
-    //         <div className="card-body">
-    //           <input type="checkbox" checked='false' className="form-check-input" id="attendingCheck" onChange={this.checkEvent} />
-    //           <label className="form-check-label" htmlFor="exampleCheck1">Attending {singleHoliday.name}?</label>
-    //         </div>
-    //       </div>,
-    //       );
-    //       console.log(friendsNotAttending);
-    //     }
-    //   });
-    // });
-
-    // const friendNames = friends.forEach((friend) => {
-    //   holidayFriends.forEach((holidayFriend) => {
-    //     if (holidayFriend.id === friend.id) {
-    //       console.log('match');
-    //       return (
-    //       <div key={friend.id} className="card col-3 mt-3 mr-1">
-    //         <h5 className="card-header">{friend.name}</h5>
-    //         <div className="card-body">
-    //           <input type="checkbox" checked='true' className="form-check-input" id="attendingCheck" onChange={this.checkEvent} />
-    //           <label className="form-check-label" htmlFor="exampleCheck1">Attending {singleHoliday.name}?</label>
-    //         </div>
-    //       </div>
-    //       );
-    //     }
-    //   });
-    // });
-
-    const friendNames = friends.map((friend, i) => (
-      holidayFriends.map((holidayFriend) => {
-        console.log(friend, holidayFriend);
-        if (friend.id === holidayFriend.id) {
-          return (
-          <div key={i} className="card col-3 mt-3 mr-1">
-            <h5 className="card-header">{friend.name}</h5>
-            <div className="card-body">
-              <input type="checkbox" checked='true' className="form-check-input" id="attendingCheck" onChange={this.checkEvent} />
-              <label className="form-check-label" htmlFor="exampleCheck1">Attending {singleHoliday.name}?</label>
-            </div>
-          </div>
-          );
-        }
-        return (
-          <div key={friend.id} className="card col-3 mt-3 mr-1">
-            <h5 className="card-header">{friend.name}</h5>
-            <div className="card-body">
-              <input type="checkbox" checked='false' className="form-check-input" id="attendingCheck" onChange={this.checkEvent} />
-              <label className="form-check-label" htmlFor="exampleCheck1">Attending {singleHoliday.name}?</label>
-            </div>
-          </div>
-        );
-      })
+    const friendNames = holidayFriends.map(holidayFriend => (
+      <div key={holidayFriend.id} className="card col-4 mt-3 mr-1">
+        <h5 className="card-header">{holidayFriend.name}</h5>
+        <div className="card-body">
+          <input type="checkbox" checked='true' className="form-check-input" id="attendingCheck" onChange={this.checkEvent} />
+          <label className="form-check-label" htmlFor="exampleCheck1">Attending {singleHoliday.name}?</label>
+        </div>
+      </div>
     ));
 
-    // const friendNames = friends.map(friend => (
-    //   <div key={friend.id} className="card col-3 mt-3 mr-1">
-    //     <h5 className="card-header">{friend.name}</h5>
-    //     <div className="card-body">
-    //     </div>
-    //   </div>
-    // ));
+    const notFriendNames = notHolidayFriends.map(notHolidayFriend => (
+      <div key={notHolidayFriend.id} className="card col-4 mt-3 mr-1">
+        <h5 className="card-header">{notHolidayFriend.name}</h5>
+        <div className="card-body">
+          <input type="checkbox" className="form-check-input" id="attendingCheck" onChange={this.checkEvent} />
+          <label className="form-check-label" htmlFor="exampleCheck1">Attending {singleHoliday.name}?</label>
+        </div>
+      </div>
+    ));
 
     return (
       <div className="holiday-friends mx-auto mt-5">
         <h3>Add Friends to {singleHoliday.name}</h3>
         <div className="row justify-content-center">{friendNames}</div>
+        <div className="row justify-content-center">{notFriendNames}</div>
       </div>
     );
   }
